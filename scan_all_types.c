f96719d948b576316c7b4f1388e8cf2329c67d2c
Laborde Camille <labordecam@pc-cy301-013.cpi.cergy.eisti.fr>
2022-05-31 11:51:29 +0200

    scan text() scan_word() et scan_long()

--- scan_all_types.c
+++ scan_all_types.c
@@ -1,47 +1,136 @@
 #include "headers.h"
 
-char* scan_text(char* ask, char* error, int taille_max){
-	//Statement :
-	char text[taille_max];
-	char text2[taille_max];
-	int okay;
-	char i;
+void scan_text(char* ask, char* text, int taille_max){
+	//
 
-	//Initialization :
-	okay =0;
-	i =0;
+	//Statement & Initialization :
+	int check;
+	int i;
+	char* error_char ="Vous ne pouvez pas saisir des charactères différents des lettres\n";
+
 
 	do{
-		memset(text2, '\0', sizeof(text));
+		text[0] ='\0';
+		check =0;
+		i =0;
+
+		//Enter text
 		printf("%s\n", ask);
-		strcpy(text, fgets(text, taille_max, stdin));
-		//fgets(text, taille_max, stdin);
+		fgets(text, taille_max, stdin);
 
-		printf("Voici le texte : %s\n", text);
-		printf("Voici le text2 : %s\n", text2);
-		//printf("strlen(text) :%d\n", strlen(text2));
+		//Delete the '\n' character
+		do{
+			i ++;
+		} while(text[i] !='\n');
+		text[i] ='\0';
 
-		strncpy(text2, text, strlen(text2));
-
-		printf("Voici le texte modifié : %s\n", text2);
-		/*for(i =0; i <strlen(text); i++){
-			if(text[i] <65 || (text[i] >90 && text[i] <96) || text[i] >122){
-				//printf("Voici le texte : %s\n", text);
-				okay =1;
-				printf("not oki\n");
+		//Check if only letters have been given
+		for(i =0; i <strlen(text); i++){
+			if((text[i] <65 && text[i] !=32) || (text[i] >90 && text[i] <96) || text[i] >122){
+				check =1;
 			}
 		}
 
-		if (okay !=0){
-			printf("%s\n", error);
-		}*/
-	} while(okay !=0);
-	return text;
+		//Explain the problem if there is one
+		if (check !=0){
+			printf(ANSI_COLOR_RED "%s\n" ANSI_COLOR_RESET, error_char);
+		}
+
+	} while(check !=0);
 }
 
+void scan_word(char* ask, char* word, int taille_max){
+	//
+
+	//Statement & Initialization :
+	int check;
+	int i;
+	char* error_space ="Vous ne pouvez pas saisir d'espaces\n";
+
+
+	do{
+		word[0] ='\0';
+		check =0;
+		i =0;
+
+		//Enter text
+		printf("%s\n", ask);
+		fgets(word, taille_max, stdin);
+
+		//Delete the '\n' character
+		do{
+			i ++;
+		} while(word[i] !='\n');
+		word[i] ='\0';
+
+		//Check if space (' ') has been given
+		for(i =0; i <strlen(word); i++){
+			if(word[i] ==' '){
+				check =1;
+			}
+		}
+
+		//Explain the problem if there is one
+		if (check !=0){
+			printf(ANSI_COLOR_RED "%s\n" ANSI_COLOR_RESET, error_space);
+		}
+
+	} while(check !=0);
+}
+
+long int scan_long(char* ask, char* lg, int taille_max){
+	//
+
+	//Statement & Initialization :
+	int check;
+	int i;
+	char* error_char ="Vous ne pouvez pas saisir des charactères différents des chiffres\n";
+
+
+	do{
+		lg[0] ='\0';
+		check =0;
+		i =0;
+
+		//Enter text
+		printf("%s\n", ask);
+		fgets(lg, taille_max, stdin);
+
+		//Delete the '\n' character
+		do{
+			i ++;
+		} while(lg[i] !='\n');
+		lg[i] ='\0';
+
+		//Check if only numbers have been given
+		for(i =0; i <strlen(lg); i++){
+			if(lg[i] >'9' || lg[i] <'0'){
+				check =1;
+			}
+		}
+
+		//Explain the problem if there is one
+		if (check !=0){
+			printf(ANSI_COLOR_RED "%s\n" ANSI_COLOR_RESET, error_char);
+		}
+
+	} while(check !=0);
+
+	return strtol(lg, NULL, 10);
+}
+
+/*
 int main(){
-	char* text;
-	text =scan_text("Saisir nom auteur", "ous ne pouvez pas saisir des charactères différents des lettres", 50);
-	//printf("Voici le texte : %s\n", text);
+	//Exemples of how to use all functions in this file.
+
+	//Statement & Initialization :
+	char text[SIZE_MAX];
+	long res =0;
+
+	//scan_text("Saisir nom auteur", text, SIZE_MAX);
+	//scan_word("Saisir un login", text, SIZE_MAX);
+	//res =scan_long("Saisir id book", text, SIZE_MAX);
+
 	return 0;
 }
+*/
