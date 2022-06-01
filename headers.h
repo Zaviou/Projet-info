a041a0fd1892c2fa6bd0ca3d330f38dfc2d17b92
Dupontrouve Xavier <dupontrouv@pc-cy301-014.cpi.cergy.eisti.fr>
2022-06-01 16:37:31 +0200

    prévention de conflits

--- headers.h
+++ headers.h
@@ -8,6 +8,11 @@
 #define SIZE_MAX 50
 //Colors
 #define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 typedef struct{
@@ -43,17 +48,34 @@ void get_book(Id* list_id, Books* list_book, int book_nb, int id_nb, int id_curs
 void give_book(Id* list_id, Books* list_book, int book_nb, int id_nb, int id_cursor, char* title);
 
 //display_list_book.c
+int date_comparison(long borrow_date, int type_user, int* comparison);
+/*
+	This function calculates if the user is late or not to return the books he borrowed. The time allowed to keep the book is 120 seconds for a student and 180 seconds for a teacher. It returns 1 if the user is not late and 0 if he is.
+	*/
+Books get_book_from_id (Books* list_book, char* id_book, int book_nb);
+/*
+	This function uses an ID of a book and returns the book's informations after recognizing the ID.
+	*/
 void display_book(Id* list_id ,char* login ,Books* list_book ,int id_nb, int book_nb, int* cursor_id);
+/*
+	This function prints the list of the books borrowed with the name and the author. It also prints the time remaining to return it, if the time has passed the name and author of the book are written in red.
+	
+	list_id: list of ids contained in the ID's file (id.txt).
+	login: login of the user ~~~~~
+	id_nb: 
+	book_nb:
+	*/
+void all_books(Books* list_book, int book_nb);
 
 //translate.c
 long int char_to_long(char* str);
 
 //connect.c
-int connect(Books* list_book, Id* list_id, int id_nb, int book_nb, int* cursor_id);
+int connect(Books* list_book, Id* list_id, int id_nb, int book_nb, int* cursor_id, int id_cursor, char* title);
 
 //iu.o
-void start(Books* list_book, Id* list_id, int id_nb, int book_nb, int* cursor_id);
-void book_management (Books* list_book, Id* list_id, int id_nb, int book_nb, int* cursor_id);
+void start(Books* list_book, Id* list_id, int id_nb, int book_nb, int* cursor_id,  int id_cursor, char* title);
+void book_management (Books* list_book, Id* list_id, int id_nb, int book_nb, int* cursor_id, int id_cursor, char* title);
 
 //scan_all_types.c
 void scan_text(char* ask, char* text, int taille_max);
